7628b5a35da624522254dd4e4b3b50ceb6832cda
XingHy7066 <zrxp7066@leeds.ac.uk>
2025-03-04 15:22:55 +0000

    3

--- recaman.py
+++ recaman.py
@@ -1,13 +1,17 @@
 L = int(input("Length of sequence: "))
 
-sequence = [0]
 
-for k in range(1, L):
-    prev = sequence[k-1]
-    next_value = prev - k
-    if next_value > 0 and next_value not in sequence:
-        sequence.append(next_value)
-    else:
-        sequence.append(prev + k)
+if L <= 0:
+    sequence = []
+else:
+    sequence = [0]
 
-[print(item) for item in sequence]
+    for k in range(1, L):
+        prev = sequence[k-1]
+        next_value = prev - k
+        if next_value > 0 and next_value not in sequence:
+            sequence.append(next_value)
+        else:
+            sequence.append(prev + k)
+
+print(sequence)
